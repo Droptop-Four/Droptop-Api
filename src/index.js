b906ed14c07a930eb9a030d1197ac23778b93b20
Bunz <66bunz@gmail.com>
2024-03-05 09:35:49 +0100

    Added ping & verison endpoint

--- src/index.js
+++ src/index.js
@@ -27,8 +27,8 @@ router.get('/', () => {
 			error: {
 				type: 'Bad Request',
 				status: 400,
-				message: 'You need to specify the api version.'
-			}
+				message: 'You need to specify the api version.',
+			},
 		}),
 		{ status: 400 }
 	);
@@ -58,7 +58,7 @@ router.get(`${apiVersion}/changelog/:version`, async ({ req }) => {
 		return new Response(JSON.stringify(changelogData.changelog));
 	}
 
-	const changenote = changelogData.changelog.find(entry => entry.version === version);
+	const changenote = changelogData.changelog.find((entry) => entry.version === version);
 
 	if (!changenote) {
 		return new Response(
@@ -66,8 +66,8 @@ router.get(`${apiVersion}/changelog/:version`, async ({ req }) => {
 				error: {
 					type: 'Not found',
 					status: 404,
-					message: `The changenote with the '${version}' version does not exist.`
-				}
+					message: `The changenote with the '${version}' version does not exist.`,
+				},
 			}),
 			{ status: 404 }
 		);
@@ -81,8 +81,8 @@ router.get(`${apiVersion}/community-apps/id`, async () => {
 	return new Response(null, {
 		status: 303,
 		headers: {
-			Location: '/v1/community-apps/'
-		}
+			Location: '/v1/community-apps/',
+		},
 	});
 });
 
@@ -91,8 +91,8 @@ router.get(`${apiVersion}/community-apps/name`, async () => {
 	return new Response(null, {
 		status: 303,
 		headers: {
-			Location: '/v1/community-apps/'
-		}
+			Location: '/v1/community-apps/',
+		},
 	});
 });
 
@@ -101,8 +101,8 @@ router.get(`${apiVersion}/community-apps/uuid`, async () => {
 	return new Response(null, {
 		status: 303,
 		headers: {
-			Location: '/v1/community-apps/'
-		}
+			Location: '/v1/community-apps/',
+		},
 	});
 });
 
@@ -127,14 +127,14 @@ router.get(`${apiVersion}/community-apps/:id`, async ({ req }) => {
 				error: {
 					type: 'Invalid id',
 					status: 400,
-					message: `The '${id}' id is not a number.`
-				}
+					message: `The '${id}' id is not a number.`,
+				},
 			}),
 			{ status: 400 }
 		);
 	}
 
-	const app = communityAppsData.apps.find(app => app.app.id === Number(id));
+	const app = communityAppsData.apps.find((app) => app.app.id === Number(id));
 
 	if (!app) {
 		return new Response(
@@ -142,8 +142,8 @@ router.get(`${apiVersion}/community-apps/:id`, async ({ req }) => {
 				error: {
 					type: 'Not found',
 					status: 404,
-					message: `The app with the '${id}' id does not exist.`
-				}
+					message: `The app with the '${id}' id does not exist.`,
+				},
 			}),
 			{ status: 404 }
 		);
@@ -162,8 +162,8 @@ router.get(`${apiVersion}/community-apps/id/:id`, async ({ req }) => {
 				error: {
 					type: 'Invalid id',
 					status: 400,
-					message: `The '${id}' id is not a number.`
-				}
+					message: `The '${id}' id is not a number.`,
+				},
 			}),
 			{ status: 400 }
 		);
@@ -172,7 +172,7 @@ router.get(`${apiVersion}/community-apps/id/:id`, async ({ req }) => {
 	const response = await fetch('https://github.com/Droptop-Four/GlobalData/raw/main/data/community_apps/community_apps.json');
 	const communityAppsData = await response.json();
 
-	const app = communityAppsData.apps.find(app => app.app.id === Number(id));
+	const app = communityAppsData.apps.find((app) => app.app.id === Number(id));
 
 	if (!app) {
 		return new Response(
@@ -180,8 +180,8 @@ router.get(`${apiVersion}/community-apps/id/:id`, async ({ req }) => {
 				error: {
 					type: 'Not found',
 					status: 404,
-					message: `The app with the '${id}' id does not exist.`
-				}
+					message: `The app with the '${id}' id does not exist.`,
+				},
 			}),
 			{ status: 404 }
 		);
@@ -197,7 +197,7 @@ router.get(`${apiVersion}/community-apps/name/:name`, async ({ req }) => {
 	const response = await fetch('https://github.com/Droptop-Four/GlobalData/raw/main/data/community_apps/community_apps.json');
 	const communityAppsData = await response.json();
 
-	const app = communityAppsData.apps.find(app => app.app.name.toLowerCase() == name.toLowerCase());
+	const app = communityAppsData.apps.find((app) => app.app.name.toLowerCase() == name.toLowerCase());
 
 	if (!app) {
 		return new Response(
@@ -205,8 +205,8 @@ router.get(`${apiVersion}/community-apps/name/:name`, async ({ req }) => {
 				error: {
 					type: 'Not found',
 					status: 404,
-					message: `The app with the "'${name}' name does not exist.`
-				}
+					message: `The app with the "'${name}' name does not exist.`,
+				},
 			}),
 			{ status: 404 }
 		);
@@ -222,7 +222,7 @@ router.get(`${apiVersion}/community-apps/uuid/:uuid`, async ({ req }) => {
 	const response = await fetch('https://github.com/Droptop-Four/GlobalData/raw/main/data/community_apps/community_apps.json');
 	const communityAppsData = await response.json();
 
-	const app = communityAppsData.apps.find(app => app.app.uuid == uuid);
+	const app = communityAppsData.apps.find((app) => app.app.uuid == uuid);
 
 	if (!app) {
 		return new Response(
@@ -230,8 +230,8 @@ router.get(`${apiVersion}/community-apps/uuid/:uuid`, async ({ req }) => {
 				error: {
 					type: 'Not found',
 					status: 404,
-					message: `The app with the '${uuid}' uuid does not exist.`
-				}
+					message: `The app with the '${uuid}' uuid does not exist.`,
+				},
 			}),
 			{ status: 404 }
 		);
@@ -245,8 +245,8 @@ router.get(`${apiVersion}/community-themes/id`, async () => {
 	return new Response(null, {
 		status: 303,
 		headers: {
-			Location: '/v1/community-themes/'
-		}
+			Location: '/v1/community-themes/',
+		},
 	});
 });
 
@@ -255,8 +255,8 @@ router.get(`${apiVersion}/community-themes/name`, async () => {
 	return new Response(null, {
 		status: 303,
 		headers: {
-			Location: '/v1/community-themes/'
-		}
+			Location: '/v1/community-themes/',
+		},
 	});
 });
 
@@ -265,8 +265,8 @@ router.get(`${apiVersion}/community-themes/uuid`, async () => {
 	return new Response(null, {
 		status: 303,
 		headers: {
-			Location: '/v1/community-themes/'
-		}
+			Location: '/v1/community-themes/',
+		},
 	});
 });
 
@@ -291,14 +291,14 @@ router.get(`${apiVersion}/community-themes/:id`, async ({ req }) => {
 				error: {
 					type: 'Invalid id',
 					status: 400,
-					message: `The '${id}' id is not a number.`
-				}
+					message: `The '${id}' id is not a number.`,
+				},
 			}),
 			{ status: 400 }
 		);
 	}
 
-	const theme = communityThemesData.themes.find(theme => theme.theme.id === Number(id));
+	const theme = communityThemesData.themes.find((theme) => theme.theme.id === Number(id));
 
 	if (!theme) {
 		return new Response(
@@ -306,8 +306,8 @@ router.get(`${apiVersion}/community-themes/:id`, async ({ req }) => {
 				error: {
 					type: 'Not found',
 					status: 404,
-					message: `The theme with the '${id}' id does not exist.`
-				}
+					message: `The theme with the '${id}' id does not exist.`,
+				},
 			}),
 			{ status: 404 }
 		);
@@ -326,8 +326,8 @@ router.get(`${apiVersion}/community-themes/id/:id`, async ({ req }) => {
 				error: {
 					type: 'Invalid id',
 					status: 400,
-					message: `The '${id}' id is not a number.`
-				}
+					message: `The '${id}' id is not a number.`,
+				},
 			}),
 			{ status: 400 }
 		);
@@ -336,7 +336,7 @@ router.get(`${apiVersion}/community-themes/id/:id`, async ({ req }) => {
 	const response = await fetch('https://github.com/Droptop-Four/GlobalData/raw/main/data/community_themes/community_themes.json');
 	const communityThemesData = await response.json();
 
-	const theme = communityThemesData.themes.find(theme => theme.theme.id === Number(id));
+	const theme = communityThemesData.themes.find((theme) => theme.theme.id === Number(id));
 
 	if (!theme) {
 		return new Response(
@@ -344,8 +344,8 @@ router.get(`${apiVersion}/community-themes/id/:id`, async ({ req }) => {
 				error: {
 					type: 'Not found',
 					status: 404,
-					message: `The theme with the '${id}' id does not exist.`
-				}
+					message: `The theme with the '${id}' id does not exist.`,
+				},
 			}),
 			{ status: 404 }
 		);
@@ -361,7 +361,7 @@ router.get(`${apiVersion}/community-themes/name/:name`, async ({ req }) => {
 	const response = await fetch('https://github.com/Droptop-Four/GlobalData/raw/main/data/community_themes/community_themes.json');
 	const communityThemesData = await response.json();
 
-	const theme = communityThemesData.themes.find(theme => theme.theme.name.toLowerCase() == name.toLowerCase());
+	const theme = communityThemesData.themes.find((theme) => theme.theme.name.toLowerCase() == name.toLowerCase());
 
 	if (!theme) {
 		return new Response(
@@ -369,8 +369,8 @@ router.get(`${apiVersion}/community-themes/name/:name`, async ({ req }) => {
 				error: {
 					type: 'Not found',
 					status: 404,
-					message: `The theme with the "'${name}' name does not exist.`
-				}
+					message: `The theme with the "'${name}' name does not exist.`,
+				},
 			}),
 			{ status: 404 }
 		);
@@ -386,7 +386,7 @@ router.get(`${apiVersion}/community-themes/uuid/:uuid`, async ({ req }) => {
 	const response = await fetch('https://github.com/Droptop-Four/GlobalData/raw/main/data/community_themes/community_themes.json');
 	const communityThemesData = await response.json();
 
-	const theme = communityThemesData.themes.find(theme => theme.theme.uuid == uuid);
+	const theme = communityThemesData.themes.find((theme) => theme.theme.uuid == uuid);
 
 	if (!theme) {
 		return new Response(
@@ -394,8 +394,8 @@ router.get(`${apiVersion}/community-themes/uuid/:uuid`, async ({ req }) => {
 				error: {
 					type: 'Not found',
 					status: 404,
-					message: `The theme with the '${uuid}' uuid does not exist.`
-				}
+					message: `The theme with the '${uuid}' uuid does not exist.`,
+				},
 			}),
 			{ status: 404 }
 		);
@@ -407,10 +407,7 @@ router.get(`${apiVersion}/community-themes/uuid/:uuid`, async ({ req }) => {
 // /v1/downloads
 router.get(`${apiVersion}/downloads`, async ({ env, req }) => {
 	const user = await login(env.REALM_APPID, env.REALM_APIKEY);
-	const collection = user
-		.mongoClient('mongodb-atlas')
-		.db(env.DB)
-		.collection(env.DROPTOP_COLLECTION);
+	const collection = user.mongoClient('mongodb-atlas').db(env.DB).collection(env.DROPTOP_COLLECTION);
 
 	try {
 		const downloadsDocument = await collection.findOne({ title: 'downloads' });
@@ -427,8 +424,8 @@ router.get(`${apiVersion}/downloads`, async ({ env, req }) => {
 				error: {
 					type: 'Something went wrong',
 					status: 500,
-					message: error.message
-				}
+					message: error.message,
+				},
 			}),
 			{ status: 500 }
 		);
@@ -442,8 +439,8 @@ router.get(`${apiVersion}/downloads/community-apps`, async ({ env, req }) => {
 			error: {
 				type: 'Bad Request',
 				status: 400,
-				message: 'You need to specify an uuid.'
-			}
+				message: 'You need to specify an uuid.',
+			},
 		}),
 		{ status: 400 }
 	);
@@ -455,8 +452,8 @@ router.post(`${apiVersion}/downloads/community-apps`, async ({ env, req }) => {
 			error: {
 				type: 'Bad Request',
 				status: 400,
-				message: 'You need to specify an uuid.'
-			}
+				message: 'You need to specify an uuid.',
+			},
 		}),
 		{ status: 400 }
 	);
@@ -467,10 +464,7 @@ router.get(`${apiVersion}/downloads/community-apps/:uuid`, async ({ env, req }) 
 	const uuid = req.params.uuid;
 
 	const user = await login(env.REALM_APPID, env.REALM_APIKEY);
-	const collection = user
-		.mongoClient('mongodb-atlas')
-		.db(env.DB)
-		.collection(env.APPS_COLLECTION);
+	const collection = user.mongoClient('mongodb-atlas').db(env.DB).collection(env.APPS_COLLECTION);
 
 	try {
 		const app = await collection.findOne({ uuid: uuid });
@@ -481,8 +475,8 @@ router.get(`${apiVersion}/downloads/community-apps/:uuid`, async ({ env, req }) 
 					error: {
 						type: 'Not found',
 						status: 404,
-						message: `The app with the '${uuid}' uuid does not exist.`
-					}
+						message: `The app with the '${uuid}' uuid does not exist.`,
+					},
 				}),
 				{ status: 404 }
 			);
@@ -490,7 +484,7 @@ router.get(`${apiVersion}/downloads/community-apps/:uuid`, async ({ env, req }) 
 
 		const app_data = {
 			uuid: app.uuid,
-			downloads: app.downloads
+			downloads: app.downloads,
 		};
 
 		return new Response(JSON.stringify(app_data));
@@ -502,8 +496,8 @@ router.get(`${apiVersion}/downloads/community-apps/:uuid`, async ({ env, req }) 
 				error: {
 					type: 'Something went wrong',
 					status: 500,
-					message: error.message
-				}
+					message: error.message,
+				},
 			}),
 			{ status: 500 }
 		);
@@ -516,10 +510,7 @@ router.post(`${apiVersion}/downloads/community-apps/:uuid`, async ({ env, req })
 	console.log(uuid);
 
 	const user = await login(env.REALM_APPID, env.REALM_APIKEY);
-	const collection = user
-		.mongoClient('mongodb-atlas')
-		.db(env.DB)
-		.collection(env.APPS_COLLECTION);
+	const collection = user.mongoClient('mongodb-atlas').db(env.DB).collection(env.APPS_COLLECTION);
 
 	try {
 		const app = await collection.findOne({ uuid: uuid });
@@ -527,13 +518,13 @@ router.post(`${apiVersion}/downloads/community-apps/:uuid`, async ({ env, req })
 		if (!app) {
 			await collection.insertOne({
 				uuid: uuid,
-				downloads: 1
+				downloads: 1,
 			});
 			let downloads = 1;
 
 			const app_data = {
 				uuid: uuid,
-				downloads: downloads
+				downloads: downloads,
 			};
 
 			return new Response(JSON.stringify(app_data));
@@ -545,7 +536,7 @@ router.post(`${apiVersion}/downloads/community-apps/:uuid`, async ({ env, req })
 
 			const app_data = {
 				uuid: app.uuid,
-				downloads: downloads
+				downloads: downloads,
 			};
 
 			return new Response(JSON.stringify(app_data));
@@ -558,8 +549,8 @@ router.post(`${apiVersion}/downloads/community-apps/:uuid`, async ({ env, req })
 				error: {
 					type: 'Something went wrong',
 					status: 500,
-					message: error.message
-				}
+					message: error.message,
+				},
 			}),
 			{ status: 500 }
 		);
@@ -573,8 +564,8 @@ router.get(`${apiVersion}/downloads/community-themes`, async ({ env, req }) => {
 			error: {
 				type: 'Bad Request',
 				status: 400,
-				message: 'You need to specify an uuid.'
-			}
+				message: 'You need to specify an uuid.',
+			},
 		}),
 		{ status: 400 }
 	);
@@ -586,8 +577,8 @@ router.post(`${apiVersion}/downloads/community-themes`, async ({ env, req }) => 
 			error: {
 				type: 'Bad Request',
 				status: 400,
-				message: 'You need to specify an uuid.'
-			}
+				message: 'You need to specify an uuid.',
+			},
 		}),
 		{ status: 400 }
 	);
@@ -598,10 +589,7 @@ router.get(`${apiVersion}/downloads/community-themes/:uuid`, async ({ env, req }
 	const uuid = req.params.uuid;
 
 	const user = await login(env.REALM_APPID, env.REALM_APIKEY);
-	const collection = user
-		.mongoClient('mongodb-atlas')
-		.db(env.DB)
-		.collection(env.THEMES_COLLECTION);
+	const collection = user.mongoClient('mongodb-atlas').db(env.DB).collection(env.THEMES_COLLECTION);
 
 	try {
 		const theme = await collection.findOne({ uuid: uuid });
@@ -612,8 +600,8 @@ router.get(`${apiVersion}/downloads/community-themes/:uuid`, async ({ env, req }
 					error: {
 						type: 'Not found',
 						status: 404,
-						message: `The theme with the '${uuid}' uuid does not exist.`
-					}
+						message: `The theme with the '${uuid}' uuid does not exist.`,
+					},
 				}),
 				{ status: 404 }
 			);
@@ -621,7 +609,7 @@ router.get(`${apiVersion}/downloads/community-themes/:uuid`, async ({ env, req }
 
 		const theme_data = {
 			uuid: theme.uuid,
-			downloads: theme.downloads
+			downloads: theme.downloads,
 		};
 
 		return new Response(JSON.stringify(theme_data));
@@ -633,8 +621,8 @@ router.get(`${apiVersion}/downloads/community-themes/:uuid`, async ({ env, req }
 				error: {
 					type: 'Something went wrong',
 					status: 500,
-					message: error.message
-				}
+					message: error.message,
+				},
 			}),
 			{ status: 500 }
 		);
@@ -645,10 +633,7 @@ router.post(`${apiVersion}/downloads/community-themes/:uuid`, async ({ env, req 
 	const uuid = req.params.uuid;
 
 	const user = await login(env.REALM_APPID, env.REALM_APIKEY);
-	const collection = user
-		.mongoClient('mongodb-atlas')
-		.db(env.DB)
-		.collection(env.THEMES_COLLECTION);
+	const collection = user.mongoClient('mongodb-atlas').db(env.DB).collection(env.THEMES_COLLECTION);
 
 	try {
 		const theme = await collection.findOne({ uuid: uuid });
@@ -656,13 +641,13 @@ router.post(`${apiVersion}/downloads/community-themes/:uuid`, async ({ env, req 
 		if (!theme) {
 			await collection.insertOne({
 				uuid: uuid,
-				downloads: 1
+				downloads: 1,
 			});
 			let downloads = 1;
 
 			const theme_data = {
 				uuid: uuid,
-				downloads: downloads
+				downloads: downloads,
 			};
 
 			return new Response(JSON.stringify(theme_data));
@@ -674,7 +659,7 @@ router.post(`${apiVersion}/downloads/community-themes/:uuid`, async ({ env, req 
 
 			const theme_data = {
 				uuid: theme.uuid,
-				downloads: downloads
+				downloads: downloads,
 			};
 
 			return new Response(JSON.stringify(theme_data));
@@ -687,12 +672,30 @@ router.post(`${apiVersion}/downloads/community-themes/:uuid`, async ({ env, req 
 				error: {
 					type: 'Something went wrong',
 					status: 500,
-					message: error.message
-				}
+					message: error.message,
+				},
 			}),
 			{ status: 500 }
 		);
 	}
+});
+
+// /v1/ping
+router.get(`${apiVersion}/ping`, () => {
+	return new Response(
+		JSON.stringify({
+			version: 'v1',
+			timestamp: new Date().toISOString(),
+		})
+	);
+});
+
+// /v1/version
+router.get(`${apiVersion}/version`, async () => {
+	const response = await fetch('https://raw.githubusercontent.com/Droptop-Four/GlobalData/main/data/version.json');
+	const versionData = await response.json();
+
+	return new Response(JSON.stringify(versionData));
 });
 
 router.any('*', () => {
@@ -701,8 +704,8 @@ router.any('*', () => {
 			error: {
 				type: 'Not Found',
 				status: 404,
-				message: '404, not found!'
-			}
+				message: '404, not found!',
+			},
 		}),
 		{ status: 404 }
 	);
@@ -711,5 +714,5 @@ router.any('*', () => {
 export default {
 	async fetch(request, env, ctx) {
 		return router.handle(request, env, ctx);
-	}
+	},
 };
